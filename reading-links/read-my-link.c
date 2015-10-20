3f97f4094ed874d04433f70b2ff9bc33c0c32e9c
ben <ben.spoor@gmail.com>
2015-10-20 21:28:31 +0200

    Fixed the bug, in this buggy demo, there still is one!

--- reading-links/read-my-link.c
+++ reading-links/read-my-link.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_BUF_SIZE (100)
+#define MAX_BUF_SIZE (20)
 #define FIRST_FILE_NAME "foo.0"
 #define SECOND_FILE_NAME "foo.1"
 #define MY_SYM_LINK "foo"
@@ -18,6 +18,7 @@ int main()
     FILE *f1 = fopen(SECOND_FILE_NAME, "w"); 
 
     /* Create symlink to first file: foo_link --> foo.0 */
+    remove(MY_SYM_LINK);
     symlink(FIRST_FILE_NAME, MY_SYM_LINK);
 
     /* Switch symlink from first to second file foo.0 --> foo.1 */
@@ -46,22 +47,26 @@ void switch_sym_link(const char* file_name_ptr)
    int bufsize = 0;
    int file_index = 0;
 
-   memset(buf, atoi("9"),MAX_BUF_SIZE); 
-
+   /* Get the file where thesymlink points to */
    bufsize = readlink(file_name_ptr, buf, MAX_BUF_SIZE);
+
+   /* readlink returns a buffer which is not \0 ended */
    buf[bufsize+1] = '\0';
 
    printf("Currently pointing to %s\n", buf);
 
+   /* Cannot create symlink when it already exists */
    remove(file_name_ptr);
 
    /* +1 to skip over dot */
-   int index_of_int = strlen(file_name_ptr+1); 
+   int index_of_int = strlen(file_name_ptr)+1; 
 
    /* Convert integer extension chars to int */
    file_index = atoi(buf + index_of_int);
 
-   sprintf(buf, "%s.%d", file_name_ptr, ++file_index);
+   /* Create the new filename to point to and create the link */
+   file_index++;
+   sprintf(buf, "%s.%d", file_name_ptr, file_index);
    symlink(buf, file_name_ptr);
 
    printf("Now pointing to %s\n", buf);
